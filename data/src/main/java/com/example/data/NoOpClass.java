60adfd20dba6e90bc6b0fac62d5f49307d3ad3ed
Phuong Luu Hoang <phoang@vubiquity.com>
2019-04-17 07:32:17 -0400

    Enable entity scanning
    The entity classes are in different package and project. This prevents the expense-tracker from detecting these classes. I have to specifically tell Spring about it.

---
+++ data/src/main/java/com/example/data/NoOpClass.java
@@ -0,0 +1,8 @@
+package com.example.data;
+
+/**
+ * This allows the scanning of the classes in this package.
+ */
+public class NoOpClass {
+
+}
--- expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
@@ -1,13 +1,16 @@
 package com.example.expensetracker;
 
+import com.example.data.NoOpClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = NoOpClass.class)
 public class ExpenseTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenseTrackerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ExpenseTrackerApplication.class, args);
+  }
 
 }
