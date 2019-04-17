f1022c10b624f4676e0906bea1190123a3a8cc63
Phuong Luu Hoang <phoang@vubiquity.com>
2019-04-17 09:40:17 -0400

    Enable scanning of JPA repositories

--- expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
@@ -4,13 +4,14 @@ import com.example.data.NoOpClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = NoOpClass.class)
+@EnableJpaRepositories(basePackageClasses = NoOpClass.class)
 public class ExpenseTrackerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ExpenseTrackerApplication.class, args);
   }
-
 }
