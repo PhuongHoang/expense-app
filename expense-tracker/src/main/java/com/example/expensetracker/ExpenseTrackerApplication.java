d27d3aa7b7b02c35fb14ab6aab2a1cb46716040f
Phuong Luu Hoang <phoang@vubiquity.com>
2019-04-22 16:41:32 -0400

    Remove unused imports

--- expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
@@ -3,12 +3,8 @@ package com.example.expensetracker;
 import com.example.data.NoOpClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackageClasses = NoOpClass.class)
-//@EntityScan(basePackageClasses = NoOpClass.class)
-//@EnableJpaRepositories(basePackageClasses = NoOpClass.class)
 public class ExpenseTrackerApplication {
 
   public static void main(String[] args) {
