d6fd38df533ab67ec7fc8a8d7278e32aa04a35c0
Phuong Luu Hoang <phoang@vubiquity.com>
2019-04-21 23:12:41 -0400

    Delegate the entities configuration for data project
    Delegate the necessary configuration of the data project to the data project itself.

--- expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan(basePackageClasses = NoOpClass.class)
-@EnableJpaRepositories(basePackageClasses = NoOpClass.class)
+@SpringBootApplication(scanBasePackageClasses = NoOpClass.class)
+//@EntityScan(basePackageClasses = NoOpClass.class)
+//@EnableJpaRepositories(basePackageClasses = NoOpClass.class)
 public class ExpenseTrackerApplication {
 
   public static void main(String[] args) {
